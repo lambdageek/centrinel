88fb2a8ca0367d1f28f53524e1b4f3be122b5a4d
Aleksey Kliger <aleksey@xamarin.com>
2017-02-08 18:59:58 -0500

    Add HasRegionScheme, PointerRegionScheme classes, declPtrRegion func

--- src/HeapGuard/NakedPointer.hs
+++ src/HeapGuard/NakedPointer.hs
@@ -1,2 +1,88 @@
 module HeapGuard.NakedPointer where
 
+import HeapGuard.RegionInferenceResult
+import HeapGuard.Region
+
+import qualified Language.C.Analysis.SemRep as C
+import qualified Language.C.Data.Ident as C
+
+class Monad m => RegionResultMonad m where
+  rrStructTagRegion :: StructTagRef -> m RegionScheme
+  rrLookupTypedef :: C.Ident -> m C.TypeDef
+
+class HasRegionScheme t where
+  getRegionScheme :: RegionResultMonad m => t -> m RegionScheme
+
+instance HasRegionScheme StructTagRef where
+  getRegionScheme = rrStructTagRegion
+
+
+instance HasRegionScheme C.CompTypeRef where
+  getRegionScheme ctr =
+    case ctr of
+      C.CompTypeRef _sueref C.UnionTag _ni -> return PolyRS
+      C.CompTypeRef sueref C.StructTag _ni -> getRegionScheme (StructTagRef sueref)
+
+instance HasRegionScheme C.TypeName where
+  getRegionScheme tn =
+    case tn of
+      C.TyVoid -> return PolyRS
+      C.TyIntegral {} -> return PolyRS
+      C.TyFloating {} -> return PolyRS
+      C.TyComplex {} -> return PolyRS
+      C.TyComp ctr -> getRegionScheme ctr
+      C.TyEnum {} -> return PolyRS
+      C.TyBuiltin {} -> return PolyRS
+
+instance HasRegionScheme C.TypeDefRef where
+  getRegionScheme tdr =
+    case tdr of
+      C.TypeDefRef _ident (Just ty) _ni -> getRegionScheme ty
+      C.TypeDefRef ident Nothing _ni -> rrLookupTypedef ident >>= getRegionScheme
+
+instance HasRegionScheme C.TypeDef where
+  getRegionScheme td =
+    case td of
+      C.TypeDef _ident ty _attrs _ni -> getRegionScheme ty
+
+instance HasRegionScheme C.Type where
+  getRegionScheme ty =
+    case ty of
+      C.DirectType tn _qs _attrs -> getRegionScheme tn
+      C.PtrType _ty _qs _attrs -> return PolyRS
+      C.ArrayType _ty _sz _qs _attrs -> return PolyRS
+      C.FunctionType _ft _attrs -> return PolyRS
+      C.TypeDefType tdr _qs _attrs -> getRegionScheme tdr
+
+
+class PointerRegionScheme t where
+  -- | If @t@ is a pointer type @ty *@ (or a typedef thereof), return @Just rs@ wregion scheme
+  -- of @ty@. Otherwise return @Nothing@
+  pointerRegionScheme :: RegionResultMonad m => t -> m (Maybe RegionScheme)
+
+instance PointerRegionScheme C.Type where
+  pointerRegionScheme ty =
+    case ty of
+      C.DirectType {} -> return Nothing
+      C.PtrType ty' _qs _attrs -> Just <$> getRegionScheme ty'
+      C.ArrayType {} -> return Nothing -- FIXME: decay to ptr?
+      C.FunctionType {} -> return Nothing
+      C.TypeDefType tdr _qs _attrs -> pointerRegionScheme tdr
+
+instance PointerRegionScheme C.TypeDefRef where
+  pointerRegionScheme tdr =
+    case tdr of
+      C.TypeDefRef _ident (Just ty) _ni -> pointerRegionScheme ty
+      C.TypeDefRef ident Nothing _ni -> rrLookupTypedef ident >>= pointerRegionScheme
+
+instance PointerRegionScheme C.TypeDef where
+  pointerRegionScheme td =
+    case td of
+      C.TypeDef _ident ty _attrs _qs -> pointerRegionScheme ty
+
+-- | @declPtrRegion d = Just rs@ if the 'Declaration' @d@ has pointer type @ty *@ and
+-- @ty@ has region scheme @rs@.  Otherwise returns @Nothing@.
+declPtrRegion :: (RegionResultMonad m, C.Declaration d) => d -> m (Maybe RegionScheme)
+declPtrRegion = pointerRegionScheme . C.declType
+
+    

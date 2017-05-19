70d8d6771671136963b13ff8779f823f6b33f53f
Aleksey Kliger <aleksey@lambdageek.org>
2017-05-19 14:43:40 -0400

    LocalSymtabT a few more mtl class instances

--- src/Centrinel/Control/Monad/LocalSymtab.hs
+++ src/Centrinel/Control/Monad/LocalSymtab.hs
@@ -1,14 +1,21 @@
 -- | Provides a monad transformer that has a local def table.
 -- An instance of 'CM.MonadSymtab' and 'H.HasDefTable'
-{-# language GeneralizedNewtypeDeriving #-}
+{-# language GeneralizedNewtypeDeriving,
+      UndecidableInstances,
+      StandaloneDeriving
+  #-}
 module Centrinel.Control.Monad.LocalSymtab (
   LocalSymtabT(..)
   , evalLocalSymtabT
   ) where
 
+import Control.Monad.Reader.Class
+import Control.Monad.Writer.Class
 import Control.Monad.State.Class
 import Control.Monad.State.Strict (StateT (..), evalStateT)
 import Control.Monad.Trans (MonadTrans (..))
+
+import Centrinel.Control.Monad.Class.RegionResult
 
 import qualified Language.C.Analysis.DefTable as CDT
 import qualified Language.C.Analysis.TravMonad as CM
@@ -16,6 +23,10 @@ import qualified Language.C.Analysis.TravMonad as CM
 -- TODO: move this to a separate file
 newtype LocalSymtabT m a = LocalSymtabT { unLocalSymtabT :: StateT CDT.DefTable m a }
   deriving (Functor, Applicative, Monad)
+
+deriving instance MonadReader r m => MonadReader r (LocalSymtabT m)
+deriving instance MonadWriter w m => MonadWriter w (LocalSymtabT m)
+deriving instance RegionResultMonad m => RegionResultMonad (LocalSymtabT m)
 
 instance Monad m => CM.MonadSymtab (LocalSymtabT m) where
   getDefTable = LocalSymtabT get

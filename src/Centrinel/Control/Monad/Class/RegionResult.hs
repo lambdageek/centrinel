71cbca9604a21a75cf26112e6738a99b50954879
Aleksey Kliger <aleksey@lambdageek.org>
2017-05-19 14:44:16 -0400

    RegionResultMonad a few more transformers instances

--- src/Centrinel/Control/Monad/Class/RegionResult.hs
+++ src/Centrinel/Control/Monad/Class/RegionResult.hs
@@ -8,6 +8,7 @@ module Centrinel.Control.Monad.Class.RegionResult (
 import Control.Monad.Reader (ReaderT)
 import Control.Monad.Writer (WriterT)
 import Control.Monad.Except (ExceptT)
+import Control.Monad.State.Strict (StateT)
 import Control.Monad.Trans.Class (MonadTrans(..))
 
 import qualified Language.C.Data.Ident as C
@@ -25,6 +26,10 @@ instance (Monoid w, RegionResultMonad m) => RegionResultMonad (WriterT w m) wher
   rrLookupTypedef = lift . rrLookupTypedef
 
 instance RegionResultMonad m => RegionResultMonad (ReaderT r m) where
+  rrStructTagRegion = lift . rrStructTagRegion
+  rrLookupTypedef = lift . rrLookupTypedef
+
+instance RegionResultMonad m => RegionResultMonad (StateT s m) where
   rrStructTagRegion = lift . rrStructTagRegion
   rrLookupTypedef = lift . rrLookupTypedef
 

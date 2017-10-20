f64eb77f8b30689fa5e4a539c2d22ecfc1ed14a6
Aleksey Kliger <alklig@microsoft.com>
2017-10-20 16:49:56 -0400

    Add "--use-cc CC" command line option
    To override default compiler (value of CC env var or 'cc' if unset)

--- main/Main.hs
+++ main/Main.hs
@@ -10,11 +10,13 @@ main = execParser theParser >>= Centrinel.Main.main
 theParser :: ParserInfo Centrinel.Main.CentrinelCmd
 theParser =
   let
-    commonOptions = pure Centrinel.Main.CentrinelOptions
+    commonOptions = Centrinel.Main.CentrinelOptions <$> useCCOption
 
-    runProject :: Parser Centrinel.Main.CentrinelCmd
-    runProject = Centrinel.Main.RunProjectCentrinelCmd <$> commonOptions  <*> strOption (long "project" <> help "Run the analysis on every file in the compilation database JSON_FILE" <> metavar "JSON_FILE")
-    runOne :: Parser Centrinel.Main.CentrinelCmd
-    runOne = Centrinel.Main.RunOneCentrinelCmd <$> commonOptions <*> some (strArgument $ metavar "-- CFLAGS... CFILE" <> help "Run the analysis on a single C input file with the given flags")
-    parser = helper <*> (runProject <|> runOne)
+    useCCOption = optional $ strOption (long "use-cc" <> metavar "CC" <> help "Use the given C compiler for preprocessing (Default: CC environment value or 'cc' if unset)")
+
+    runProject :: Parser (Centrinel.Main.CentrinelOptions -> Centrinel.Main.CentrinelCmd)
+    runProject = flip Centrinel.Main.RunProjectCentrinelCmd <$> strOption (long "project" <> help "Run the analysis on every file in the compilation database JSON_FILE" <> metavar "JSON_FILE")
+    runOne :: Parser (Centrinel.Main.CentrinelOptions -> Centrinel.Main.CentrinelCmd)
+    runOne = flip Centrinel.Main.RunOneCentrinelCmd <$> some (strArgument $ metavar "-- CFLAGS... CFILE" <> help "Run the analysis on a single C input file with the given flags")
+    parser = helper <*> commonOptions <**> (runProject <|> runOne)
   in info parser (fullDesc <> (progDesc "centrinel is a C static analyzer"))

862bb6516d85edf9cde203802ed790b330dd2995
Aleksey Kliger <alklig@microsoft.com>
2017-10-31 11:49:54 -0400

    [Report] Refactor into a separate module for JSON

--- src/Centrinel/Report.hs
+++ src/Centrinel/Report.hs
@@ -1,18 +1,13 @@
 {-# language DeriveGeneric, OverloadedStrings #-}
 module Centrinel.Report where
 
-import GHC.Generics (Generic)
 import qualified System.IO as IO
 import Control.Exception (bracket_)
-
-import Data.Aeson (ToJSON)
-import qualified Data.Aeson
-import qualified Data.ByteString.Lazy as BS
-import qualified Data.ByteString.Lazy.Char8 as BS8
-
 import Control.Monad.Except
 
 import Centrinel.Types
+import Centrinel.Report.Types
+import qualified Centrinel.Report.Json as J
 
 -- | Describes the methods of presenting the analysis results
 data OutputMethod = OutputMethod
@@ -63,22 +58,13 @@ withOutputMethod :: OutputMethod -> (OutputFn -> IO a) -> IO a
 withOutputMethod (OutputMethod dest fmt) =
   let (acquire, release, present) = case fmt of
         PlainTextOutputFormat -> (const (return ()), const (return ()), plainTextOutput)
-        JSONOutputFormat -> (jsonAcquire, jsonRelease, jsonOutput)
+        JSONOutputFormat -> (J.header, J.footer, J.output)
       runKont isFile h kont =
         bracket_ (acquire h) (release h) (kont $ present isFile h)
   in case dest of
     StdOutOutputDestination -> runKont False IO.stdout
     FilePathOutputDestination fp -> \kont -> IO.withFile fp IO.AppendMode $ \h ->
       runKont True h kont
-
-
-data Message = Normal !String
-  | Abnormal !String
-  | Verbose !Bool !String
-  deriving (Generic)
-
-instance ToJSON Message where
-  toEncoding = Data.Aeson.genericToEncoding Data.Aeson.defaultOptions
 
 plainTextOutput :: Bool -> IO.Handle -> Message -> IO ()
 plainTextOutput isFile h = \msg ->
@@ -87,17 +73,3 @@ plainTextOutput isFile h = \msg ->
     Abnormal txt -> IO.hPutStrLn h txt
     Verbose suppress txt -> when (not suppress || isFile) $ IO.hPutStrLn h txt
 
-
-jsonOutput :: Bool -> IO.Handle -> Message -> IO ()
-jsonOutput _isFile h = \msg -> do
-    BS.hPut h (Data.Aeson.encode msg)
-    BS8.hPutStrLn h ","
-
-jsonAcquire :: IO.Handle -> IO ()
-jsonAcquire h = do
-  BS8.hPutStr h "{\"centrinel_report_version\": \"0\","
-  BS8.hPutStr h " \"messages\": [\n"
-
-jsonRelease :: IO.Handle -> IO ()
-jsonRelease h =
-  BS8.hPutStrLn h "{}\n]}" -- need a trailing empty object because of the trailing comma, in jsonOutput

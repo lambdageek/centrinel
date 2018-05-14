e9a557d14adf39bc22bff444ba10cbe9d040e932
Aleksey Kliger <alklig@microsoft.com>
2018-05-14 19:41:52 -0400

    Simplify PointerRegionAnalysisT

--- src/Centrinel/PointerRegionAnalysis.hs
+++ src/Centrinel/PointerRegionAnalysis.hs
@@ -22,52 +22,27 @@ import Centrinel.Region.Region (RegionScheme)
 import Centrinel.RegionInferenceResult
 import qualified Centrinel.Region.Unification as U
 import qualified Centrinel.Region.Unification.Term as U
-import Centrinel.Warning (hgWarn)
 
-type RegionIdentMap = Map.Map HGId.RegionIdent U.RegionUnifyTerm
+type RegionIdentMap = Map.Map HGId.RegionIdent U.RegionVar
 
 -- | Monad transformer that adds pointer region analysis on top of any underlying monad.
 newtype PointerRegionAnalysisT m a = PointerRegionAnalysisT { unPointerRegionAnalysisT :: StateT RegionIdentMap (U.UnifyRegT m) a }
-  deriving (Functor, Applicative, Monad)
+  deriving (Functor, Applicative, Monad
+           , AM.MonadName, AM.MonadSymtab, AM.MonadCError
+           , U.RegionUnification, U.ApplyUnificationState)
 
 instance MonadTrans PointerRegionAnalysisT where
-  lift m = PointerRegionAnalysisT (lift $ lift m)
-
-deriving instance AM.MonadName m => AM.MonadName (PointerRegionAnalysisT m)
-
-deriving instance AM.MonadSymtab m => AM.MonadSymtab (PointerRegionAnalysisT m)
-
-instance AM.MonadCError m => AM.MonadCError (PointerRegionAnalysisT m) where
-  throwTravError = lift . AM.throwTravError
-  catchTravError (PointerRegionAnalysisT c) handler = PointerRegionAnalysisT (AM.catchTravError c (unPointerRegionAnalysisT . handler))
-  recordError = lift . AM.recordError
-  getErrors = lift AM.getErrors
-
-deriving instance AM.MonadCError m => U.RegionUnification (PointerRegionAnalysisT m)
-
-deriving instance AM.MonadCError m => U.ApplyUnificationState (PointerRegionAnalysisT m)
-
-(-:=) :: (AM.MonadCError m, U.RegionUnification m) => U.RegionVar -> Maybe U.RegionUnifyTerm -> m U.RegionUnifyTerm
-v -:= Nothing = return (U.regionUnifyVar v)
-v -:= Just r = do
-  AM.catchTravError (U.sameRegion (U.regionUnifyVar v) r)
-    (\_err -> do
-        AM.recordError (hgWarn "failed to unify regions" Nothing) -- TODO: region info
-        return (U.regionUnifyVar v))
-
-getRegionIdent :: Monad m => HGId.RegionIdent -> PointerRegionAnalysisT m (Maybe (U.RegionUnifyTerm))
-getRegionIdent i = PointerRegionAnalysisT $ State.gets (Map.lookup i)
-
-putRegionIdent :: Monad m => HGId.RegionIdent -> U.RegionUnifyTerm -> PointerRegionAnalysisT m ()
-putRegionIdent i m = PointerRegionAnalysisT $ State.modify' (Map.insert i m)
+  lift = PointerRegionAnalysisT . lift . lift
 
 instance AM.MonadCError m => HGId.RegionAssignment (PointerRegionAnalysisT m) where
-  assignRegion i = do
-    v <- U.newRegion
-    r <- getRegionIdent i
-    r' <- v -:= r
-    putRegionIdent i r'
-    return v
+  assignRegion i = PointerRegionAnalysisT $ do
+    m <- State.gets (Map.lookup i)
+    case m of
+      Nothing -> do
+        v <- U.newRegion
+        State.modify' (Map.insert i v)
+        return v
+      Just v -> return v
 
 -- | Gets a mapping of the region identifiers that have been noted by
 -- unification to their 'RegionScheme' as implied by the constraints available
@@ -75,9 +50,9 @@ instance AM.MonadCError m => HGId.RegionAssignment (PointerRegionAnalysisT m) wh
 frozenRegionUnificationState :: AM.MonadCError m => PointerRegionAnalysisT m (Map.Map SUERef RegionScheme)
 frozenRegionUnificationState = do
   sueRegions <- PointerRegionAnalysisT $ State.gets munge
-  traverse (fmap U.extractRegionScheme . U.applyUnificationState) sueRegions
+  traverse (fmap U.extractRegionScheme . U.applyUnificationState . U.regionUnifyVar) sueRegions
   where
-    munge :: Map.Map HGId.RegionIdent U.RegionUnifyTerm -> Map.Map SUERef U.RegionUnifyTerm
+    munge :: RegionIdentMap -> Map.Map SUERef U.RegionVar
     munge = Map.mapKeysMonotonic onlySUERef . Map.filterWithKey (\k -> const (isStructTag k))
     onlySUERef :: HGId.RegionIdent -> SUERef
     onlySUERef (HGId.StructTagId sue) = sue
